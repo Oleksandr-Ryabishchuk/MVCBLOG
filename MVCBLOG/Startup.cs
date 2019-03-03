427d35864265f04ee075ed5ea5680141f1220c5f
ryabishcuk2 <ryabishcuk2@gmail.com>
2019-03-03 18:28:01 +0200

    Add project files.

---
+++ MVCBLOG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBLOG.Startup))]
+namespace MVCBLOG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
